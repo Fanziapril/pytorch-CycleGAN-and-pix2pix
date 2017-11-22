0b6c04926a61108f05e94117a1cb7e0e2d5d4956
Fanziapril <wufanzi412@gmail.com>
2017-11-22 20:47:55 +0800

    bug fix

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -31,9 +31,10 @@ class Pix2PixModel(BaseModel):
                                           opt.which_model_netD,
                                           opt.n_layers_D, opt.norm, use_sigmoid, self.gpu_ids)
         if not self.isTrain or opt.continue_train:
-            self.load_network(self.netG, 'G', opt.which_epoch)
+            save_dir = os.path.join(opt.checkpoints_dir, opt.name)
+            self.load_network(self.netG, 'G', opt.which_epoch, save_dir)
             if self.isTrain:
-                self.load_network(self.netD, 'D', opt.which_epoch)
+                self.load_network(self.netD, 'D', opt.which_epoch, save_dir)
 
         if self.isTrain:
             self.fake_AB_pool = ImagePool(opt.pool_size)
